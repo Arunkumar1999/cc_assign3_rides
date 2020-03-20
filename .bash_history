dc267183ee5ffbb1a2ca225c296c6add57033219
Ubuntu <ubuntu@ip-172-31-35-185.ec2.internal>
2020-03-20 19:30:04 +0000

    rides apis

---
+++ .bash_history
@@ -0,0 +1,315 @@
+sudo apt install nginx -y
+nginx
+sudo systectl status nginx
+sudo systemctl status nginx
+sudo apt install nginx -y
+sudo apt-get update
+sudo apt install nginx -y
+nginx
+sudo systemctl status nginx 
+clear
+cd /var/www/html
+ls
+sudo nano index.nginx-debian.html 
+cd /var/www/html
+sudo nano index.nginx-debian.html 
+cd /var/www/html
+sudo nano index.html
+ls
+sudo nano index.nginx-debian.html 
+cd /var/www/html
+ls
+cat index.
+cat index.html
+cd /var/www/html
+sudo nano index.html
+ls
+mkdir rides
+cd rides/
+sudo nano Dockerfile
+ls
+sudo nano requirements.txt
+ls
+cd rides/
+ls
+git clone https://github.com/Arunkumar1999/CC_ASSIGN_3.git
+ls
+cd CC_ASSIGN_3/
+ls
+rm user_management.py 
+ls
+cd rides
+ls
+cd ..
+pwd 
+cd CC_ASSIGN_3/
+cd rides/
+mv AreaNameEnum.csv /home/ubuntu/rides
+cd ..
+ls
+cd CC_ASSIGN_3/
+cd rides/
+mv ride_management.py /home/ubuntu/rides
+ls
+cd ..
+rm rides/
+rmdir rides/
+ls
+cd 
+cd rides/
+rmdir CC_ASSIGN_3/
+cd CC_ASSIGN_3/
+ls
+cd ..
+rmdir CC_ASSIGN_3/
+cd C
+cd CC_ASSIGN_3/
+ls
+rm *
+ls
+cd ..
+ls
+rmdir CC_ASSIGN_3/
+exit
+ls
+cd rides/
+ls
+sudo nano ride_management.py 
+docker --version
+docker ps
+sudo apt install docker.io
+docker --version
+clear
+ls
+cd rides/
+ls
+docker ps
+sudo docker ps
+docker --version
+clear
+docker build
+docker build --help
+docker build -f rides
+clear
+docker build -t rides
+docker build -f .
+docker build -f /home/ubuntu/rides .
+sudo docker build -f /home/ubuntu/rides .
+sudo docker build -f /home/ubuntu/rides/Dockerfile .
+docker ps
+sudo docker ps
+sudo docker ps -a
+sudo docker run 
+docker run --help
+Unpacking cgroupfs-mount (1.4) ...
+Selecting previously unselected package runc.
+Preparing to unpack .../3-runc_1.0.0~rc10-0ubuntu1~18.04.2_amd64.deb ...
+docker images
+sudo docker images
+sudo docker run  f88b2f81f83a 
+sudo docker ps
+cd rides/
+sudo docker images
+cd rides/
+ls
+sudo docker images
+sudo docker run -d f88b2f81f83a
+sudo docker ps
+sudo docker container -a
+sudo docker ps -a
+ls
+cd rides/
+sudo nano Dockerfile 
+ls
+sudo nano docker-compose.yml
+sudo docker-compose build 
+sudo apt install docker-compose
+sudo docker-compose build 
+sudo apt install docker-compose
+sudo docker-compose build 
+sudo docker-compose up -d
+sudo systemctl stop nginx
+sudo docker-compose up -d
+sudo docker ps
+ls
+cd rides/
+ls
+cd CC_ASSIGN_3/
+ls
+cd ..
+docker ps
+sudo docker ps
+ls
+l
+ls
+cd rides/
+ls
+sudo nano ride_management.py 
+cd ..
+sudo docker-compose build 
+sudo docker-compose up -d
+ls
+cd rides/
+ls
+clear
+ls
+cd rides
+sudo docker ps
+ls
+cd rides/
+ls
+sudo docker ps -a
+cd ..
+ls
+sudo nano docker-compose.yml 
+ls
+sudo docker-compose build
+sudo docker-compose up -d
+sudo docker ps
+ls
+cd rides
+ls
+sudo nano r
+ls
+sudo nano ride_management.py
+cd ..
+ls
+sudo docker-compose build
+sudo docker-compose up -d
+ls
+cd rides/
+ls
+sudo nano ride_management.py
+ls
+sudo docker-compose build 
+sudo docker-compose up
+sudo docker-compose up -d
+ls
+cd rides/
+ls
+sudo nano ride_management.py
+cd ..
+sudo docker-compose build 
+sudo docker-compose up -d
+sudo docker-compose build 
+sudo docker-compose up 
+clear
+ls
+cd rides/
+sudo nano ride_management.py
+ls
+cd rides/
+ls
+sudo nano ride_management.py
+cd ..
+sudo docker-compose build
+sudo docker-compose up -d
+cd rides/
+ls
+sudo nano ride_management.py
+ls
+cd rides/
+ls
+cd ..
+ls
+sudo docker-compose build
+sudo docker-compose up -d
+sudo docker exec -it rides /bin/bash
+sudo docker-compose build
+sudo docker-compose up -d
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up -d
+sudo docker exec -it rides /bin/bash
+ls
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up 
+sudo docker-compose build
+sudo docker-compose up 
+clear
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up 
+ls
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up 
+sudo docker ps
+sudo pa
+sudo ps
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up 
+ls
+ls
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up 
+ls
+sudo systemctl stop nginx
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up -d
+sudo apt-get install curl
+sudo docker ps
+IPADDR=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' aabbccddeeff)
+sudo IPADDR=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' aabbccddeeff)
+sudo IPADDR=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' rides)
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 25ac93c0f8cd 
+sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 25ac93c0f8cd 
+curl -v http://${172.21.0.2}:8080/health
+sudo $IPADDR=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 25ac93c0f8cd) 
+curl -v http://172.21.0.2:80/health
+LS
+ls
+cd rides/
+ls
+sudo nano Dockerfile 
+ls
+sudo nano Dockerfile 
+cd ..
+sudo nano docker-compose.yml 
+clear
+ls
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up
+sudo docker-compose down
+sudo docker ps
+ls
+sudo service nginx status
+sudo service nginx stop
+sudo service nginx status
+sudo docker-compose up -d
+docekr ps
+sudo docker-compose build
+sudo docker-compose up -d
+sudo docker ps
+ls
+cd rides/
+ls
+sudo nano ride_management.py
+sudo docker-compose build
+sudo docker-compose up -d
+sudo docker -sp
+sudo docker ps
+sudo docker logs --details rides
+ls
+sudo docker ps
+sudo docker logs --details users
+docker ps
+sudo docker
+sudo docker logs --details rides
+ls
+sudo nano ./rides/ride_management.py
+ls
+ls
+cd rides/
+sudo nano ride_management.py
+ls
+cd 
+sudo docker ps
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up
---
+++ rides/ride_management.py
@@ -0,0 +1,410 @@
+import sqlite3
+from flask import Flask, render_template,jsonify,request,abort,Response
+import requests
+import json
+import csv
+from datetime import datetime
+
+app=Flask(__name__)
+cursor = sqlite3.connect("rideshare_of_rides.db")
+file=open("text.txt","w")
+file.write("%d %d"%(0,0))
+file.close()
+def fun():
+
+	try:
+		file=open("text.txt","r")
+		#print(file.readline()[0])
+		e=file.readline()
+		q=int(e[0])+1
+		print(q,e[1])
+		file.close()
+		file=open("text.txt","w")
+		file.write("%d %s"%(q,e[2]))		
+		#print("as")
+		#file.close()
+	except:
+		file=open("text.txt","w")
+		file.write("%d %d"%(0,0))		
+		#print("read")
+	file.close()
+
+#fun()
+cursor.execute("""
+        CREATE TABLE IF NOT EXISTS rideusers(
+         id int not null,
+  		 name varchar(20),
+		primary key(id,name)
+        );
+    """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS place(
+      id int primary key,
+	  name varchar(20)
+    );
+""")
+
+with open('AreaNameEnum.csv') as File:  
+	reader = csv.reader(File)
+
+	i=0
+	for row in reader:
+		if(i):
+			try:
+				d=[row[0],row[1]]
+				sql="insert into place values (?,?)"
+				
+				cursor.execute(sql,d)
+			except:
+				continue	
+		i=1	
+
+cursor.commit()
+
+cursor.execute("""
+        CREATE TABLE IF NOT EXISTS rides(
+          rideid integer  primary key AUTOINCREMENT,
+          name varchar(20) not null ,
+  		  timest DATETIME not null,
+  		  source varchar(30) not null,
+  		  desti varchar(30) not null
+        );
+""")
+
+cursor.commit()
+
+@app.route("/", methods=["GET"])
+def hello():
+     return "<h1>Hello</h1>"
+
+@app.route("/api/v1/db/read",methods=["POST"])
+def read_database():
+	cursor = sqlite3.connect("rideshare_of_rides.db")
+	resp_dict={}
+	val=request.get_json()["insert"]
+	table=request.get_json()["table"]
+	column=request.get_json()["column"]
+	where_check_cond=request.get_json()["where"]
+	if(len(where_check_cond)>0):
+		check_string=""
+		for i in range(len(where_check_cond)-1):
+			check_string+=where_check_cond[i]+" = "+"'"+val[i]+"'"+" AND "
+		check_string+=where_check_cond[len(where_check_cond)-1]+" = "+"'"+val[len(where_check_cond)-1]+"'"
+		##print(check_string,"aaaaaaaaaaaaa")
+				
+
+	r=""
+	s=""
+	e=len(column)-1
+	for i in range(e):
+		r+=column[i]+","
+		s+="?,"
+	r+=column[e]
+	s+="?"
+	for i in range(len(val)):
+		val[i]=val[i].encode("utf8")
+
+	if(len(where_check_cond)>0):
+		sql="select "+r+" from "+table+" where "+check_string+";"
+	else:
+		sql="select "+r+" from "+table+";"
+		print(sql,"ghgghhghghghghghhg")
+	
+	
+	resp=cursor.execute(sql)
+	resp_check=resp.fetchall()
+	print(resp_check[0],"length of resp_check")
+	if(len(resp_check) == 0):
+		resp_dict["response"]=0
+		return json.dumps(resp_dict)
+	else:
+		
+		#print(resp_check)
+		#print(list(resp_check[0]))
+		#print(len(resp_check),"count of all rows")
+		resp_dict["count"]=resp_check[0]
+		for i in range(len(resp_check)):
+			for j in range(len(column)):
+				resp_dict.setdefault(column[j],[]).append(list(resp_check[i])[j])
+		#print(resp_dict,"hii i am dict")
+		#print("user does exists from read_Db")
+		resp_dict["response"]=1
+		return json.dumps(resp_dict)
+
+@app.route("/api/v1/db/write",methods=["POST"])
+def to_database():
+	
+	indicate=request.get_json().get("indicate")
+	#print("indicate:", indicate)
+	try :
+		cursor = sqlite3.connect("rideshare_of_rides.db")
+		cursor.execute("PRAGMA FOREIGN_KEYS=on")
+		cursor.commit()
+	except Exception as e:
+		#print("Database connect error:",e)
+		pass
+	if(indicate=="0"):
+		val=request.get_json().get("insert")
+		table=request.get_json().get("table")
+		column=request.get_json().get("column")
+		#print("val:",val)
+		#print("table",table)
+		#print("column:", column)
+		r=""
+		s=""
+		e=len(column)-1
+		for i in range(e):	
+			r+=column[i]+","
+			s+="?,"
+		r+=column[e]
+		s+="?"
+		for i in range(len(val)):
+			val[i]=val[i]
+
+		try:
+
+			sql="insert into "+table+" ("+r+")"+" values ("+s+")"
+			#print("query:",sql)
+			cursor.execute(sql,val)
+
+			cursor.commit()
+			# sql="select * from "+table
+			# et=cursor.execute(sql)
+			# rows = et.fetchall()
+
+			# sql="select * from users"
+			# et=cursor.execute(sql)
+			# rows = et.fetchall()
+			#return jsonify(1)
+		except Exception as e:
+			#print(e)
+			sql="select * from "+table
+			et=cursor.execute(sql)
+			rows = et.fetchall()
+			#for row in rows:
+				#print(row,"we")
+			return jsonify(0)
+		return jsonify(1)
+	elif(indicate=='1'):
+		table=request.get_json()["table"]
+		delete=request.get_json()["delete"]
+		column=request.get_json()["column"]
+		#print("table",table)
+		#print("delete:",delete)
+		try:
+			#print("asdf")
+			sql="select * from "+table+" WHERE "+column+"=(?)"
+			#print("query",sql)
+			et=cursor.execute(sql,(delete,))
+			if(not et.fetchone()):
+				#print("fs")
+				return jsonify(0)
+			
+			sql = "DELETE from "+table+" WHERE "+column+"=(?)"
+			#print(table,column,delete)
+			#print(sql)
+			et=cursor.execute(sql,(delete,))
+			#print(et.fetchall())
+			cursor.commit()
+		except Exception as e:
+			#print(e)
+			#print("rt")
+			return jsonify(0)
+		return jsonify(1)
+	elif(indicate=='3'):
+		try:
+			et=cursor.execute("DELETE FROM rides")
+			cursor.execute("DELETE FROM rideusers")
+			cursor.commit()
+		except Exception as e:
+			return jsonify(0)
+		return jsonify(1)
+
+
+
+	else:
+		return jsonify(0)
+
+
+@app.route("/api/v1/rides",methods=["POST"])
+def insert_rider():
+	fun()
+	if(request.method!="POST"):
+		abort(405,"method not allowed")
+	global rides_id
+	
+	name=request.get_json()["created_by"]
+	timestamp=request.get_json()["timestamp"]
+	source=request.get_json()["source"]
+	destination=request.get_json()["destination"]
+    
+	d=[name,timestamp,source,destination]
+	#print(type(timestamp.encode("utf8")))
+	#print(timestamp.encode("utf8"))
+	try:
+		time_object=datetime.strptime(timestamp,'%d-%m-%Y:%S-%M-%H')
+	except:
+		#print("hii")
+		abort(400,"invalid input")
+
+	read_res=requests.get("http://CC-Project-1507675041.us-east-1.elb.amazonaws.com:80/api/v1/users",headers={"Origin":"3.84.169.225"})
+	if(read_res.json()==0):
+		abort(400,"no users to fetch")
+	if(name not in read_res.json()):
+		abort(400,"user doesn't exist")
+	
+	res=requests.post("http://127.0.0.1:80/api/v1/db/write",json={"insert":d,"column":["name","timest","source","desti"],"table":"rides","indicate":"0"})	
+	if(res.json()==0):
+		abort(400,"user already exists")
+
+	return Response("success",status=201,mimetype='application/json')
+
+
+@app.route("/api/v1/rides/<rideId>",methods=["DELETE"])
+def delete_rideId(rideId):
+	fun()
+	if(request.method!="DELETE"):
+		abort(405,"method not allowed")
+	
+	res=requests.post("http://127.0.0.1:80/api/v1/db/write",json={"table":"rides","delete":rideId,"column":"rideid","indicate":"1"})
+	if(res.json()==0):
+		abort(400,"rideId does not  exists")
+	elif(res.json()==1):
+		return json.dumps({'success':"deleted successfully"}), 200, {'ContentType':'application/json'}
+
+@app.route("/api/v1/rides/<rideId>",methods=["POST"])
+def join_ride(rideId):
+	fun()
+	if(request.method!="POST"):
+		abort(405,"method not allowed")
+	
+	name=request.get_json()["username"]
+	d=[name,rideId]
+	#read_res=requests.post("http://172.17.0.1:8000/api/v1/db/read",json={"insert":d,"column":["name","pass"],"table":"users","where":["name"]})
+	read_res=requests.get("http://CC-Project-1507675041.us-east-1.elb.amazonaws.com:80/api/v1/users")
+
+	if(read_res.json()==0):
+		abort(400,"no user to fetch")
+	if(name not in read_res.json()):
+		abort(400,"user doesn't exist")
+	d=[rideId,name]
+	rideid_check=requests.post("http://127.0.0.1:80/api/v1/db/read",json={"insert":d,"column":["rideid","name","source","desti"],"table":"rides","where":["rideid"]})
+	if(rideid_check.json().get("response")==0):
+		abort(400,"ride id doesn't exists")
+	
+	res=requests.post("http://127.0.0.1:80/api/v1/db/write",json={"insert":d,"column":["id","name"],"table":"rideusers","indicate":"0"})
+	if(res.json()==0):
+		abort(400,"rideId does not  exists")
+	elif(res.json()==1):
+		return json.dumps({'success':"joined successfully"}), 200, {'ContentType':'application/json'}
+
+
+@app.route("/api/v1/rides/<rideId>",methods=["GET"])
+def ride_details(rideId):
+	fun()
+	if(request.method!="GET"):
+		abort(405,"method not allowed")
+	users=""
+	
+	d=[rideId]
+	user_list=[]
+	rideid_check=requests.post("http://127.0.0.1:80/api/v1/db/read",json={"insert":d,"column":["rideid","name","source","desti","timest"],"table":"rides","where":["rideid"]})
+	if(rideid_check.json().get("response")==0):
+		abort(400,"rideId does not  exists")
+	elif(rideid_check.json().get("response")==1):
+		
+		joined_users_check=requests.post("http://127.0.0.1:80/api/v1/db/read",json={"insert":d,"column":["id","name"],"table":"rideusers","where":["id"]})
+
+
+		return json.dumps({"rideId":rideid_check.json().get("rideid"),
+							"created_by":rideid_check.json().get("name")[0],
+							"users":joined_users_check.json().get("name"),
+							"timestamp":rideid_check.json().get("timest"), 
+							"source":rideid_check.json().get("source"),
+							"destination":rideid_check.json().get("desti")}),200, {'ContentType':'application/json'}
+
+@app.route("/api/v1/rides",methods=["GET"])
+def upcoming_rides():
+	fun()
+	if(request.method!="GET"):
+		abort(405,"method not allowed")
+	array_of_rides=[]
+	
+	dateTimeObj = datetime.now()
+	string=""
+	string+=str(dateTimeObj.day)+"-"+str(dateTimeObj.month)+"-"+str(dateTimeObj.year)+":"+str(dateTimeObj.second)+"-"+str(dateTimeObj.minute)+"-"+str(dateTimeObj.hour)
+	source=request.args.get('source')
+	destination=request.args.get('destination')
+	d=[source,destination]				
+	src_dest_check=requests.post("http://127.0.0.1:80/api/v1/db/read",json={"insert":d,"column":["rideid","name","source","desti","timest"],"table":"rides","where":['source','desti']})			
+	if(src_dest_check.json().get("response")==0):
+		return "no content to send",204,{"ContentType":"application/json"}
+	
+	str_timestmp=src_dest_check.json().get("timest")
+	timestamp1=src_dest_check.json().get("timest")
+	for i in range(len(timestamp1)):
+		t1 = datetime.strptime(timestamp1[i], "%d-%m-%Y:%S-%M-%H")
+		t2 = datetime.strptime(string, "%d-%m-%Y:%S-%M-%H")
+		if(t1>t2):
+			array_of_rides.append({"rideId":src_dest_check.json().get("rideid")[i],
+								"username":src_dest_check.json().get("name")[i],
+								"timestamp":src_dest_check.json().get("timest")[i]})
+
+	return json.dumps(array_of_rides),200,{"ContentType":"application/json"}
+
+
+@app.route("/api/v1/db/clear",methods=["POST"])
+def clear_db():
+	if(request.method!="POST"):
+		abort(405,"method not allowed")
+	
+	res=requests.post("http://127.0.0.1:80/api/v1/db/write",json={"indicate":"3"})	
+	if(res.json()==0):
+		abort(400,"failed to clear")
+	elif(res.json()==1):
+		return json.dumps({'success':"cleared successfully"}), 200, {'ContentType':'application/json'}
+	
+@app.route("/api/v1/_count",methods=["GET"])
+def get_http_request():
+	if(request.method!="GET"):
+		abort(405,"method not allowed")
+	try:
+		file=open("text.txt","r")
+		e=file.readline()
+		file.close()
+	except:
+		return json.dumps([0]),200, {'ContentType':'application/json'}	
+	return json.dumps([int(e[0])]),200, {'ContentType':'application/json'}
+
+@app.route("/api/v1/_count",methods=["DELETE"])
+def clear_http_request():
+	if(request.method!="DELETE"):
+		abort(405,"method not allowed")
+	
+	file=open("text.txt","w")
+	file.write("%d %d"%(0,0))		
+
+	file.close()
+	return json.dumps({'success':"cleared successfully"}), 200, {'ContentType':'application/json'}
+
+@app.route("/api/v1/rides/count",methods=["GET"])
+def ride_count():
+	fun()
+	if(request.method!="GET"):
+		abort(405,"method not allowed")
+	users=""
+	
+	#d=[rideId]
+	user_list=[]
+	rideid_check=requests.post("http://127.0.0.1:80/api/v1/db/read",json={"insert":[],"column":["count(*) as count"],"table":"rides","where":[]})
+	
+	if(rideid_check.json().get("response")==1):
+		print(rideid_check.json().get("count"),"ssss")
+		return json.dumps(rideid_check.json().get("count")),200, {'ContentType':'application/json'}
+
+
+
+if __name__ == '__main__':
+	app.debug=True
+	app.run(host='0.0.0.0',port=80)
